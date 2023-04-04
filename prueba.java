a749798e65919a1ca221f94ec02966504e47e240
juanesrp <juanesrp@gmail.com>
2023-04-03 20:25:03 -0500

    Primer commit

---
+++ prueba.java
@@ -0,0 +1,14 @@
+package OB;
+
+public class prueba {
+
+    public static void main(String[] args) {
+        suma(10, 30);
+    }
+    
+    public static void suma(int a,int b){
+        int resultado= 0;
+        resultado= a+b;
+        System.out.println(resultado);
+    }
+}
