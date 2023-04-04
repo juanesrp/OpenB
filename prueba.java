cf19028e0a88106671c529d1530fa40bbb2751dc
juanesrp <juanesrp@gmail.com>
2023-04-03 20:43:36 -0500

    Resolucion ejercio 3 de funciones

--- prueba.java
+++ prueba.java
@@ -3,12 +3,40 @@ package OB;
 public class prueba {
 
     public static void main(String[] args) {
-        suma(10, 30);
+        suma(10, 30,20);
     }
     
-    public static void suma(int a,int b){
+    public static void suma(int a,int b,int c){
         int resultado= 0;
-        resultado= a+b;
+        resultado= a+b+c;
         System.out.println(resultado);
+    }
+}
+
+class Coche {
+    private int numPuertas;
+
+    public Coche(int numPuertas) {
+        this.numPuertas = numPuertas;
+    }
+
+    public int getNumPuertas() {
+        return numPuertas;
+    }
+
+    public void setNumPuertas(int numPuertas) {
+        this.numPuertas = numPuertas;
+    }
+
+    public void agregarPuerta() {
+        this.numPuertas++;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Coche miCoche = new Coche(3);
+        miCoche.agregarPuerta();
+        System.out.println("Mi coche tiene " + miCoche.getNumPuertas() + " puertas.");
     }
 }
