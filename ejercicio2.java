b672d64f6c6539092315f567b7ebd5b6ec72cc24
juanesrp <juanesrp@gmail.com>
2023-04-18 20:37:46 -0500

    Sentencias de control y ejercicio2

---
+++ ejercicio2.java
@@ -0,0 +1,62 @@
+package OB;
+
+public class ejercicio2 {
+
+    public static void main(String[] args) {
+        
+       System.out.println("Primer ejercicio IF: ");
+       var numeroif= 5;
+       
+       if(numeroif == 0){
+        System.out.println("Es igual a 0 \n");
+       } else if(numeroif > 0){
+            System.out.println("El valor es positivo \n");
+       } else {
+            System.out.println("El valor es negativo \n");
+       }
+
+       System.out.println("Segundo ejercicio While:");
+       var numeroWhile= 0;
+       
+       while(numeroWhile < 3){
+        numeroWhile+=1;
+        System.out.println(numeroWhile);
+       }
+
+       System.out.println("\nTercer ejercicio Do While:");
+       int numeroDoWhile=2;
+
+       do{
+        numeroDoWhile++;
+        System.out.println(numeroDoWhile);
+       } while(numeroDoWhile < 3);
+
+       System.out.println("\nCuarto ejercicio For:");
+       
+       for(int numeroFor=0; numeroFor <= 3; numeroFor++){
+        System.out.println(numeroFor);
+       }
+
+       System.out.println("\nQuinto ejercicio switch:");
+       var estacion="VERANO";
+        
+        switch(estacion){
+            case "VERANO":
+                System.out.println("Es verano");
+                break;
+            case "INVIERNO":
+                System.out.println("Es invierno");
+                break;
+            case "OTOÑO":
+                System.out.println("Es Otoño");
+                break;
+            case "PRIMAVERA":
+                System.out.println("Es Primavera");
+                break;    
+            default:
+                System.out.println("Esto no es una estación");
+        } 
+
+    }
+    
+}
---
+++ sentanciasDeControl.java
@@ -0,0 +1,75 @@
+package OB;
+
+public class sentanciasDeControl {
+
+    public static void main(String[] args){
+        //IF condicionales
+        // String estacion= "primavera";
+
+        // if(estacion == "primavera"){
+        //     System.out.println("Es primavera");
+        // } else if(estacion == "Invierno"){
+        //     System.out.println("Es invierno");
+        // } else System.out.println("No es ni primavera ni invierno");
+
+        //WHILE
+        //int contador=10;
+        // while(contador>0){
+        //     System.out.println("Contador Vale: " + contador);
+        //     contador=contador-1;
+
+        //DO WHILE
+        // do{
+        //     System.out.println("Contador Vale: " + contador);
+        //     contador=contador-1;
+        // }while(contador>0);
+
+        //FOR
+        // for(int i=0; i<contador; i++){
+        //     System.out.println("Contador Vale: " + contador);
+        //     contador-= 1;
+        // }
+        // int valores[]= {10,20,30,40,50};
+        // for(int i = 0; i<valores.length; i++){
+        //     System.out.println(valores[i]);
+        // }
+        
+        //SWITCH
+        // var estacion="VERANO";
+        
+        // switch(estacion){
+        //     case "VERANO":
+        //         System.out.println("Es verano");
+        //         break;
+        //     case "INVIERNO":
+        //         System.out.println("Es invierno");
+        //         break;
+        //     default:
+        //         System.out.println("No es ni verano ni invierno");
+        // } 
+
+        var hoy_es="DOMINGO";
+
+        switch(hoy_es){
+            case "LUNES":
+                System.out.println("Laborable");
+                break;
+            case "MARTES":
+                System.out.println("Laborables");
+                break;
+            case "MIERCOLES":
+                System.out.println("Laborables");
+                break;
+            case "JUEVES":
+                System.out.println("Laborables");
+                break;
+            case "VIERNES":
+                System.out.println("Laborables");
+                break;
+            default:
+            System.out.println("No laborable");
+
+        }
+    }
+    
+}
