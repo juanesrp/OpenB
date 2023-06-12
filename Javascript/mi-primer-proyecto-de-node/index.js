e4a9bc548bbaca7eaacbc1236b3bc17809b1e27e
juanesrp <juanesrp@gmail.com>
2023-06-11 21:38:58 -0500

    Introducción a javascript, node y npm

---
+++ Javascript/mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,12 @@
+//Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+/*
+    Esta es la parte intermedia de nuestro proyecto
+    En esta parte realizamos los chequeos necesarios
+    por ultimo vamos a la ultima linea
+    Esto esta comentado
+
+
+*/
+//Este es el mensaje para despedir
+console.log("Adios");
